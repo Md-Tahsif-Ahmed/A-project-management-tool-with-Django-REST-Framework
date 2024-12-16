bb4d263d64db222b293c826bb24cd5dd875043d4
Md-Tahsif-Ahmed <tahsif.cse@gmail.com>
2024-12-16 18:57:14 +0600

    all

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,20 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-from .models import User  # Import your custom User model
+from .models import Project, Task, Comment, ProjectMember
 
-# Register the User model with a customized UserAdmin
+# Get the custom User model
+User = get_user_model()
+
+# Register custom User model with a customized UserAdmin
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    # Fields to display in the User list in the admin
+    # Fields to display in the User list
     list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
-    search_fields = ('username', 'email', 'first_name', 'last_name')  # Allow searching by username and email
+    search_fields = ('username', 'email', 'first_name', 'last_name')  # Search by username, email, etc.
     ordering = ('id',)
 
     # Fields to display when viewing/editing a User
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (None, {'fields': ('username', 'password')}), 
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}), 
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}), 
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
 
@@ -25,3 +29,31 @@ class CustomUserAdmin(UserAdmin):
             'fields': ('username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'is_staff', 'is_active'),
         }),
     )
+
+# Register Project model
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'owner', 'created_at')
+    search_fields = ('name', 'owner__username')
+    list_filter = ('created_at', 'owner')
+
+# Register Task model
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status', 'priority', 'assigned_to', 'project', 'created_at', 'due_date')
+    search_fields = ('title', 'assigned_to__username', 'project__name')
+    list_filter = ('status', 'priority', 'project', 'assigned_to')
+
+# Register Comment model
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'user', 'task', 'created_at')
+    search_fields = ('content', 'user__username', 'task__title')
+    list_filter = ('created_at', 'task')
+
+# Register ProjectMember model
+@admin.register(ProjectMember)
+class ProjectMemberAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project', 'user', 'role')
+    search_fields = ('user__username', 'project__name')
+    list_filter = ('role', 'project')
